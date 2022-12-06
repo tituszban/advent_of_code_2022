4e78859949ffb7dbc21cb416955c24820a8fead9
Titusz Ban <tituszb@hq.trayport.com>
2022-12-06 06:14:07 +0000

    Day 05

--- 05/1/main.py
+++ 05/1/main.py
@@ -1,9 +1,48 @@
+import re
+
+
+class Stacks:
+    def __init__(self, initial_layout: list[tuple[str]]):
+        stack_count = max([len(row) for row in initial_layout])
+
+        self._stacks = [
+            [
+                row[i]
+                for row in reversed(initial_layout)
+                if len(row) > i and row[i].strip()
+            ]
+
+            for i in range(stack_count)
+        ]
+
+    def move(self, count, stack_from, stack_to):
+        for _ in range(count):
+            v = self._stacks[stack_from-1].pop(-1)
+            self._stacks[stack_to-1].append(v)
+
+    def get_top(self):
+        return ''.join(s[-1] for s in self._stacks)
+
 
 def main():
     with open("05/input.txt") as f:
-        test_input = list(map(lambda l: l.strip(), f.readlines()))
+        layout, actions = list(map(lambda s: list(
+            map(lambda l: l.strip("\n"), s.split("\n"))), f.read().split("\n\n")))
+    layout_re = re.compile(r"((\s{4})|(\[[A-Z]\]\s))+")
+    actions_re = re.compile(r"move (\d+) from (\d+) to (\d+)")
 
-    
+    parsed_layout = [[line[i * 4 + 1]
+                      for i in range(len(line) // 4 + 1)] for line in layout[:-1]]
+
+    stacks = Stacks(parsed_layout)
+
+    parsed_actions = [list(map(int, actions_re.match(line).groups()))
+                      for line in actions if line]
+
+    for c, f, t in parsed_actions:
+        stacks.move(c, f, t)
+
+    print(stacks.get_top())
 
 
 if __name__ == "__main__":
