5822dff455fac08ebee54a9c3fb23be1164b70ab
Titusz Ban <tituszb@hq.trayport.com>
2022-12-06 06:14:30 +0000

    Day 06

--- 06/1/main.py
+++ 06/1/main.py
@@ -1,9 +1,19 @@
+def find_start_of_packet(chrs: str):
+    for i in range(len(chrs) - 4):
+        if len(set(chrs[i:i+4])) == 4:
+            return i + 4
+
 
 def main():
     with open("06/input.txt") as f:
         test_input = list(map(lambda l: l.strip(), f.readlines()))
 
-    
+    print(find_start_of_packet("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 7)
+    print(find_start_of_packet("bvwbjplbgvbhsrlpgdmjqwftvncz"), 5)
+    print(find_start_of_packet("nppdvjthqldpwncqszvftbrmjlhg"), 6)
+    print(find_start_of_packet("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 10)
+    print(find_start_of_packet("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 11)
+    print(find_start_of_packet(test_input[0]))
 
 
 if __name__ == "__main__":
--- 06/2/main.py
+++ 06/2/main.py
@@ -1,9 +1,19 @@
+def find_start_of_packet(chrs: str, n: int = 14):
+    for i in range(len(chrs) - n):
+        if len(set(chrs[i:i+n])) == n:
+            return i + n
+
 
 def main():
     with open("06/input.txt") as f:
         test_input = list(map(lambda l: l.strip(), f.readlines()))
 
-    
+    print(find_start_of_packet("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 19)
+    print(find_start_of_packet("bvwbjplbgvbhsrlpgdmjqwftvncz"), 23)
+    print(find_start_of_packet("nppdvjthqldpwncqszvftbrmjlhg"), 23)
+    print(find_start_of_packet("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 29)
+    print(find_start_of_packet("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 26)
+    print(find_start_of_packet(test_input[0]))
 
 
 if __name__ == "__main__":
