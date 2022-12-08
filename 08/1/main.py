b21c9a30252388cea3ca873405bf0ce60a7ea825
Titusz Ban <tituszb@hq.trayport.com>
2022-12-08 15:52:37 +0000

    Day 08

--- 08/1/main.py
+++ 08/1/main.py
@@ -1,9 +1,52 @@
+def count_visible(grid):
+    is_visible = [[False for _ in row] for row in grid]
+
+    for i, col in enumerate(grid):
+        max_val = -1
+        for j, v in enumerate(col):
+            if v > max_val:
+                max_val = v
+                is_visible[i][j] = True
+        max_val = -1
+        for j, v in reversed(list(enumerate(col))):
+            if v > max_val:
+                max_val = v
+                is_visible[i][j] = True
+    
+    is_visible_T = list(map(list, zip(*is_visible)))
+    grid_T = list(zip(*grid))
+
+    for i, col in enumerate(grid_T):
+        max_val = -1
+        for j, v in enumerate(col):
+            if v > max_val:
+                max_val = v
+                is_visible_T[i][j] = True
+        max_val = -1
+        for j, v in reversed(list(enumerate(col))):
+            if v > max_val:
+                max_val = v
+                is_visible_T[i][j] = True
+
+    return sum([sum(col) for col in is_visible_T])
+
 
 def main():
-    with open("08/input.txt") as f:
-        test_input = list(map(lambda l: l.strip(), f.readlines()))
+    example_input = """
+30373
+25512
+65332
+33549
+35390
+    """.strip().splitlines()
+    example_input = list(map(lambda v: list(map(int, v)), example_input))
 
-    
+    print(count_visible(example_input), 21)
+
+    with open("08/input.txt") as f:
+        test_input = list(map(lambda l: list(map(int, l.strip())), f.readlines()))
+
+    print(count_visible(test_input))
 
 
 if __name__ == "__main__":
--- 08/2/main.py
+++ 08/2/main.py
@@ -1,9 +1,48 @@
+def calculate_scenic_score(grid):
+    scenic_score = [[1 for _ in row] for row in grid]
+
+    for i, col in enumerate(grid):
+        scores = {k: 0 for k in range(10)}
+        for j, v in enumerate(col):
+            scenic_score[i][j] *= scores[v]
+            scores = {k: 1 if k <= v else scores[k] + 1 for k in range(10)}
+        scores = {k: 0 for k in range(10)}
+        for j, v in reversed(list(enumerate(col))):
+            scenic_score[i][j] *= scores[v]
+            scores = {k: 1 if k <= v else scores[k] + 1 for k in range(10)}
+    
+    scenic_score_T = list(map(list, zip(*scenic_score)))
+    grid_T = list(zip(*grid))
+
+    for i, col in enumerate(grid_T):
+        scores = {k: 0 for k in range(10)}
+        for j, v in enumerate(col):
+            scenic_score_T[i][j] *= scores[v]
+            scores = {k: 1 if k <= v else scores[k] + 1 for k in range(10)}
+        scores = {k: 0 for k in range(10)}
+        for j, v in reversed(list(enumerate(col))):
+            scenic_score_T[i][j] *= scores[v]
+            scores = {k: 1 if k <= v else scores[k] + 1 for k in range(10)}
+
+    return max([max(col) for col in scenic_score_T])
+
 
 def main():
-    with open("08/input.txt") as f:
-        test_input = list(map(lambda l: l.strip(), f.readlines()))
+    example_input = """
+30373
+25512
+65332
+33549
+35390
+    """.strip().splitlines()
+    example_input = list(map(lambda v: list(map(int, v)), example_input))
 
-    
+    print(calculate_scenic_score(example_input), 8)
+
+    with open("08/input.txt") as f:
+        test_input = list(map(lambda l: list(map(int, l.strip())), f.readlines()))
+
+    print(calculate_scenic_score(test_input))
 
 
 if __name__ == "__main__":
